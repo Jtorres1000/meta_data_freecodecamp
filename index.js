05350dbb214ce615f48f050907b332066e38489d
Jorge Torres <fervirlacrer@gmail.com>
2024-07-24 00:03:27 +0000

    Last changes

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
-
-var app = express();
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/',  (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
-
+app.post('/api/fileanalyse/', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  console.log(req.file)
+  if(originalname && mimetype && size) {
+    return res.json({
+      name: originalname,
+      type: mimetype,
+      size: size
+    })
+  }
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
